97cafedb2dc54913e1b8f7d172a6b57a8f82dda5
Eduardo Griesang <eduardogriesang1@gmail.com>
2024-03-18 08:25:07 -0300

    feat: unit tests

--- src/app/components/pokemom/pokemom.component.spec.ts
+++ src/app/components/pokemom/pokemom.component.spec.ts
@@ -6,10 +6,14 @@ describe('PokemomComponent', () => {
   let component: PokemomComponent;
   let fixture: ComponentFixture<PokemomComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PokemomComponent ]
+    })
+    .compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [PokemomComponent]
-    });
     fixture = TestBed.createComponent(PokemomComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +21,32 @@ describe('PokemomComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render Pokemon data', () => {
+    const pokemonData = {
+      id: 25,
+      name: 'Pikachu',
+      sprites: {
+        front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+      },
+      types: [
+        { type: { name: 'electric' } }
+      ],
+      stats: [
+        { stat: { name: 'hp' }, base_stat: 35 },
+        { stat: { name: 'attack' }, base_stat: 55 },
+        { stat: { name: 'defense' }, base_stat: 40 },
+      ],
+    };
+
+    component.pokemonData = pokemonData;
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Pikachu');
+    expect(compiled.querySelector('img').src).toContain('25.png');
+    expect(compiled.querySelector('.pokemon-type').textContent).toContain('electric');
+    expect(compiled.querySelectorAll('.pokemon-stat').length).toBe(3);
   });
 });
--- src/app/components/pokemon-input/pokemon-input.component.spec.ts
+++ src/app/components/pokemon-input/pokemon-input.component.spec.ts
@@ -1,15 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PokemonInputComponent } from './pokemon-input.component';
+import { FormsModule } from '@angular/forms';
 
 describe('PokemonInputComponent', () => {
   let component: PokemonInputComponent;
   let fixture: ComponentFixture<PokemonInputComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PokemonInputComponent ],
+      imports: [ FormsModule ]
+    })
+    .compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [PokemonInputComponent]
-    });
     fixture = TestBed.createComponent(PokemonInputComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +23,19 @@ describe('PokemonInputComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call fetchPokemon() when form is submitted', () => {
+    spyOn(component, 'fetchPokemon');
+    const form = fixture.nativeElement.querySelector('form');
+    form.dispatchEvent(new Event('submit'));
+    expect(component.fetchPokemon).toHaveBeenCalled();
+  });
+
+  it('should update pokemonName property on input change', () => {
+    const input = fixture.nativeElement.querySelector('input');
+    input.value = 'Pikachu';
+    input.dispatchEvent(new Event('input'));
+    expect(component.pokemonName).toEqual('Pikachu');
   });
 });
